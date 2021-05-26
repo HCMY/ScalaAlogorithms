c2e3d0f1041897b2aafedaba0d5ede35f897457c
HCMY <yuanchenzhouhcmy@gmail.com>
2021-05-26 16:28:02 +0800

    add stack

---
+++ src/main/scala/org/zhouyc/Stack.scala
@@ -0,0 +1,58 @@
+package org.zhouyc
+
+
+class Stack {
+  var head:Option[Node] = None
+  private var size = 0;
+
+  def clearStack(): Unit ={
+    head = None
+    size = 0
+  }
+
+  def push(data:Int){
+    var new_node = new Node(data, None)
+    head match {
+      case None =>
+        head = Some(new_node)
+      case Some(oK) =>
+        new_node.next = Some(oK)
+        head = Some(new_node)
+    }
+    size += 1
+  }
+
+  def pop(): Unit ={
+    head match {
+      case None =>
+        None
+      case Some(oK) =>
+        head = head.get.next
+        size -= 1
+    }
+  }
+
+  def top(): Option[Node] ={
+    head match {
+      case None => None
+      case Some(oK) => head
+    }
+  }
+
+  def mkString():String={
+    head.map(node=>{
+      var root = node
+      val res = new StringBuilder
+      while (root.next.isDefined){
+        res.insert(0,root.value);
+        root = root.next.get
+      }
+      res.insert(0, root.value)
+      res.mkString
+    }).getOrElse("")
+  }
+
+  def getStackSize():Int={
+    size;
+  }
+}
---
+++ src/test/scala/org/zhouyc/StackTest.scala
@@ -0,0 +1,17 @@
+package org.zhouyc
+
+object StackTest{
+  def main(args: Array[String]): Unit = {
+    val stack = new Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    println(stack.mkString())
+    println(stack.getStackSize())
+    stack.pop()
+    println(stack.getStackSize())
+    println(stack.top().get.value)
+    stack.pop()
+    println(stack.getStackSize())
+  }
+}
